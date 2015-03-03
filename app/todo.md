171fe1f4272b88cecbb264a876fb153041a7440a
Brian R. Li <brian.rusong.li@gmail.com>
2015-03-03 16:05:23 -0500

    miscellaneous fixes

---
+++ app/todo.md
@@ -0,0 +1,10 @@
+Find Better Font
+Make Logo
+Design Dashboard
+--Start Stream
+--Join Stream
+Design Homepage
+Design Login/Signup Pages
+Stream Page
+--stream, comment box
+
--- app/views.py
+++ app/views.py
@@ -1,6 +1,27 @@
 from app import app
 from flask import Flask, render_template
 
+# HOMEPAGE
 @app.route('/')
+@app.route('/index')
 def index():
   return render_template('index.html')
+
+# LOGIN
+@app.route('/login')
+def login():
+  return render_template('login.html')
+
+# DASHBOARD
+# -login dependent
+@app.route('/dashboard')
+def dashboard():
+  return render_template('dashboard.html')
+
+# COMING SOON
+@app.route('/cs')
+@app.route('/comingsoon')
+def comingsoon():
+  return render_template('comingsoon.html')
+
+
