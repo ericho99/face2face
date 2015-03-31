976244e0cbbc9f2e42f3320b6dde593a4bdccb2d
Eric Ho <eric.ho@yale.edu>
2015-03-31 12:30:09 -0400

    indent run.py using spaces

--- run.py
+++ run.py
@@ -7,9 +7,9 @@ from testing import face2faceTest
 # from flask import Flask
 
 if __name__ == '__main__':
-	cmd_args = sys.argv
-	if len(cmd_args) > 1 and cmd_args[1] == "test":
-		#do a test; facilitated by a pass right now
-		pass 
+    cmd_args = sys.argv
+    if len(cmd_args) > 1 and cmd_args[1] == "test":
+        #do a test; facilitated by a pass right now
+        pass
     app.run(debug=True)
-
+    
