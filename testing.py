96406d34ee9bbce5bfe5de2da3f5d7625811d715
David Roeca <david.roeca@yale.edu>
2015-03-31 12:07:05 -0400

    added bare bones testing code

---
+++ testing.py
@@ -0,0 +1,13 @@
+import unittest
+# import any other classes from this app that we want to test
+
+
+# go to https://github.com/davidroeca/GildedRose-Refactoring-Kata/blob/master/python/test_gilded_rose.py
+# this will help you get a sense of how to run these tests
+
+# this is bare bones code - write down some 
+class face2faceTest(unittest.TestCase):
+	def testPaypal():
+		pass
+	def testLivestream():
+		pass
