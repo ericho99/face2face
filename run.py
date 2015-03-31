d912c8ed50f359bebe0908d50ff12366d6ee3b87
David Roeca <david.roeca@yale.edu>
2015-03-31 12:55:45 -0400

    switched back the testing scheme to something more correct; still bare-bones

--- run.py
+++ run.py
@@ -1,15 +1,8 @@
 #!flask/bin/python
 from app import app
-import sys
-from testing import face2faceTest
 
 # import os
 # from flask import Flask
 
 if __name__ == '__main__':
-    cmd_args = sys.argv
-    if len(cmd_args) > 1 and cmd_args[1] == "test":
-        #do a test; facilitated by a pass right now
-        pass
     app.run(debug=True)
-    
